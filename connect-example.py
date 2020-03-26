884183ca487cbd2eb6fc1f9b0dbd5305083b2b8d
Moritz Warning <moritzwarning@web.de>
2020-03-26 18:13:41 +0100

    uses variables instead of undocumented arguments

--- connect-example.py
+++ connect-example.py
@@ -4,6 +4,9 @@ import nuki
 from nacl.public import PrivateKey
 import sys
 
+nukiMacAddress = "00:00:00:00:00:01"
+name="Test" # name that the Nuki uses for its own log
+
 # generate the private key which must be kept secret
 keypair = PrivateKey.generate()
 myPublicKeyHex = keypair.public_key.__bytes__().hex()
@@ -12,5 +15,5 @@ myID = 50
 # id-type = 00 (app), 01 (bridge) or 02 (fob)
 # take 01 (bridge) if you want to make sure that the 'new state available'-flag is cleared on the Nuki if you read it out the state using this library
 myIDType = '01'
-nuki.Nuki(sys.argv[0]).authenticateUser(myPublicKeyHex, myPrivateKeyHex, myID, myIDType, sys.argv[1])
+nuki.Nuki(nukiMacAddress).authenticateUser(myPublicKeyHex, myPrivateKeyHex, myID, myIDType, name)
 
